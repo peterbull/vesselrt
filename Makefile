5d715dd3ee0e842139ae5b4f2aa04db39ec463cf
peter bull <17122363+peterbull@users.noreply.github.com>
2026-05-03 11:31:50 -0400

    fun with waitgroups

--- Makefile
+++ Makefile
@@ -4,5 +4,14 @@ build:
 run: build
 	sudo ./bin/app run ubuntu /bin/sh
 
+build-scratch:
+	go build -o bin/scratch ./scratch
+
+
+scratch: build-scratch
+	./bin/scratch
+
 shell:
 	limactl shell dev
+
+	
---
+++ scratch/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func testFunc() int {
+	return 123
+}
+func testFunc2() {
+	fmt.Println("called testfunc2")
+}
+func DoWork(i int) int {
+	fmt.Printf("%v", i)
+	return i
+}
+func oldWg() {
+	dataChan := make(chan int)
+	testFunc()
+	testFunc2()
+	go func() {
+		wg := sync.WaitGroup{}
+		for i := range 1000 {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				dataChan <- DoWork(i)
+			}()
+		}
+		wg.Wait()
+		close(dataChan)
+	}()
+	for i := range dataChan {
+		fmt.Printf("datachan i: %v\n", i)
+	}
+}
+func newWg() {
+	dataChan := make(chan int)
+	var (
+		wg sync.WaitGroup
+	)
+
+	go func() {
+		for i := range 1000 {
+			wg.Go(func() {
+				dataChan <- DoWork(i)
+			})
+		}
+		wg.Wait()
+		close(dataChan)
+	}()
+
+	for i := range dataChan {
+		fmt.Printf("datachan i: %v\n", i)
+	}
+}
+func main() {
+	fmt.Println("scratch")
+	// oldWg()
+	newWg()
+	fmt.Println("break")
+}
